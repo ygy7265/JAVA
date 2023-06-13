904e122c2fed789ab232b8872c43725470c2fb56
parasite.ygy <parasite.ygy@gmail.com>
2023-06-13 15:27:08 +0900

    commit

---
+++ Java/Ch01/src/sub01/Hello.java
@@ -0,0 +1,26 @@
+package sub01;
+/**
+ * 날짜 : 2023/06/12
+ * 이름 : 윤경엽
+ * 내용 : Ch01.Java 개발환경구축
+ *
+ */
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+		//기본출력
+		System.out.println("Hello World~");
+		System.out.println("Hello Java~"); 
+		
+		//한줄출력
+		System.out.print("Hello World");
+		System.out.print("Korea");
+		
+		//개행처리
+		System.out.print("\n");
+		System.out.print("Hello Busan");
+		
+		
+	}
+}
